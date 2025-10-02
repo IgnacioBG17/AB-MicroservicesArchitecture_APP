4afe2f907def5522440e9e5c8943ba78a7051eb3
Angel Bustamante <bustamanteangel532@gmail.com>
2025-10-01 21:57:00 -0600

    t-AuthAPI: feat(auth): crear servicio Ecommerce.Service.Auth para autenticación de usuarios.
    - Implementación del servicio de autenticación de usuarios
    - Inclusión de métodos básicos para manejo de login, registro y validación
    - Preparación de la capa de servicios para integrarse con Identity y base de datos

---
+++ Ecommerce/Ecommerce.Services.AuthAPI/Program.cs
@@ -0,0 +1,66 @@
+using Ecommerce.Services.AuthAPI.Data;
+using Ecommerce.Services.AuthAPI.Models;
+using Ecommerce.Services.AuthAPI.Service;
+using Ecommerce.Services.AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+/* Configuración cadena de conexion */
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+/* Configuración JWT */
+builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
+
+/* Configuración de autenticación y autorización con Identity en Ecommerce.service.AuthAPI */
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+/* Configuración Swagger */
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    /* Configuración Swagger */
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseAuthentication(); /* usamos autenticación */
+app.MapControllers();
+ApplyMigration();
+app.Run();
+
+
+/* Aplicar la migración al arrancar el servicio */
+void ApplyMigration()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var _db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        if (_db.Database.GetPendingMigrations().Count() > 0)
+        {
+            _db.Database.Migrate();
+        }
+    }
+}
