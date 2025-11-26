381dd7a6e284dbc884caa422156397ae5be9aec2
Angel Bustamante <bustamanteangel532@gmail.com>
2025-11-26 16:33:36 -0600

    feat(shopping-cart): implementación completa del módulo Shopping Cart
    -Creación del proyecto ShoppingCartAPI y configuración inicial con NuGet Packages
    -Definición de modelos y DTOs para carrito, detalles y estructura de intercambio
    -Configuración base del API y creación del controlador Cart
    -Implementación del método CartUpsert con lógica para creación y actualización del carrito
    -Eliminación de detalles del carrito mediante endpoint dedicado
    -Carga completa del carrito del usuario con sus productos y cantidades
    -Integración del ProductAPI desde ShoppingCartAPI usando HttpClientFactory
    -Implementación del consumo interservicio y sincronización de datos de producto
    -Creación de endpoints para aplicar y remover cupones en el carrito
    - Integración del consumo de CouponAPI desde el carrito para aplicar descuentos

---
+++ Ecommerce/Ecommerce.Services.ShoppingCartAPI/Service/CouponService.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.Services.ShoppingCartAPI.Models.Dto;
+using Ecommerce.Services.ShoppingCartAPI.Service.IService;
+using Newtonsoft.Json;
+
+namespace Ecommerce.Services.ShoppingCartAPI.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CouponService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<CouponDto> GetCoupon(string couponCode)
+        {
+            var client = _httpClientFactory.CreateClient("Coupon");
+            var response = await client.GetAsync($"/api/coupon/GetByCode{couponCode}");
+
+            if (!response.IsSuccessStatusCode)
+                return new CouponDto();
+
+            var apiContent = await response.Content.ReadAsStringAsync();
+
+            var resp = JsonConvert.DeserializeObject<ResponseDto>(apiContent);
+
+            if (resp == null || !resp.IsSuccess)
+                return new CouponDto();
+
+            return JsonConvert.DeserializeObject<CouponDto>(Convert.ToString(resp.Result));
+        }
+    }
+}
