295f1de0e1ca6165b7b8e6b460930febf3f11430
Angel Bustamante <bustamanteangel532@gmail.com>
2025-12-05 22:51:19 -0600

    feat(order-management): Implementación completa del módulo de gestión de órdenes
    Creación de endpoints para obtener todas las órdenes y detalles individuales.
    - Implementación del endpoint para actualizar el estado de una orden.
    - Integración de nuevos endpoints en el Order Service.
    - Desarrollo del listado de órdenes con DataTables y carga dinámica desde el backend.
    - Configuración del endpoint de base de datos y ajuste de modelo para soportar estados.
    - Creación de la vista dinámica de detalles de orden.
    - Generación de botones dinámicos para actualización de estado y acciones de UI.
    - Implementación de la lógica para actualizar estados desde la interfaz.
    - Agregado de filtros de estado mediante query string.
    - Modificación del controlador para manejar filtrado por estado.
    - Configuración de toggles y filtros interactivos en la interfaz.

--- Ecommerce/Ecommerce.Services.OrderAPI/Controllers/OrderController.cs
+++ Ecommerce/Ecommerce.Services.OrderAPI/Controllers/OrderController.cs
@@ -10,6 +10,7 @@ using Ecommerce.Services.OrderAPI.Service.IService;
 using Ecommerce.Services.OrderAPI.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Stripe;
 using Stripe.Checkout;
 
@@ -38,6 +39,51 @@ namespace Ecommerce.Services.OrderAPI.Controllers
             _productService = productService;
             _messageBus = messageBus;
             _configuration = configuration;
+        }
+
+        [Authorize]
+        [HttpGet("GetOrders")]
+        public ResponseDto Get(string userId = "")
+        {
+            try
+            {
+                IEnumerable<OrderHeader> objList;
+                if (User.IsInRole(SD.RoleAdmin))
+                {
+                    objList = _db.OrderHeaders.Include(u => u.OrderDetails).OrderByDescending(u => u.OrderHeaderId).ToList();
+                }
+                else
+                {
+                    objList = _db.OrderHeaders.Include(u => u.OrderDetails).Where(u => u.UserId == userId)
+                                                .OrderByDescending(u => u.OrderHeaderId).ToList();
+                }
+                _response.Result = _mapper.Map<IEnumerable<OrderHeaderDto>>(objList);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [Authorize]
+        [HttpGet("GetOrder/{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                OrderHeader orderHeader = _db.OrderHeaders.Include(u => u.OrderDetails).First(u => u.OrderHeaderId == id);
+                _response.Result = _mapper.Map<OrderHeaderDto>(orderHeader);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
         }
 
         [Authorize]
@@ -168,6 +214,39 @@ namespace Ecommerce.Services.OrderAPI.Controllers
                 _response.IsSuccess = false;
             }
 
+            return _response;
+        }
+
+        [Authorize]
+        [HttpPost("UpdateOrderStatus/{orderId:int}")]
+        public async Task<ResponseDto> UpdateOrderStatus(int orderId, [FromBody] string newStatus)
+        {
+            try
+            {
+                OrderHeader orderHeader = _db.OrderHeaders.First(u => u.OrderHeaderId == orderId);
+                if (orderHeader != null)
+                {
+                    if (newStatus == SD.Status_Cancelled)
+                    {
+                        var options = new RefundCreateOptions
+                        {
+                            Reason = RefundReasons.RequestedByCustomer,
+                            PaymentIntent = orderHeader.PaymentIntentId
+                        };
+
+                        var service = new RefundService();
+                        Refund refund = service.Create(options);
+                    }
+
+                    orderHeader.Status = newStatus;
+                    _db.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.Message = ex.Message;
+                _response.IsSuccess = false;
+            }
             return _response;
         }
     }
